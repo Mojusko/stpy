202ec697640f038072f0254bca01f9d4b3975409
Mojusko <mojmir.mutny@inf.ethz.ch>
2025-01-07 16:25:28 +0100

    putting in the right order

--- stpy/probability/gaussian_likelihood.py
+++ stpy/probability/gaussian_likelihood.py
@@ -197,3 +197,66 @@ class GaussianLikelihood(Likelihood):
                 raise NotImplementedError("The desired confidence set type is not supported.")
 
         return beta
+    
+
+     def confidence_parameter_prior_posterior(self, delta,params):
+        H = params['regularizer_hessian']
+        print ("hessian", H)
+        sigma = params['sigma']
+        n = self.x.size()[0]
+        x = self.x @ torch.sqrt(torch.inverse(H).double())
+        K = (x@x.T + (sigma**2)*torch.eye(n))
+        evidence_of_the_data = -0.5*self.y.T@torch.linalg.solve(K,self.y)-0.5*torch.linalg.slogdet(K)[1]-(n/2)*np.log(2*np.pi) + (n/2)*np.log(2*np.pi*sigma**2) ## last terms come from the other
+        return np.log(1./delta) - evidence_of_the_data
+
+
+    def confidence_parameter_prior_posterior_weighted(self, delta,params):
+        H = params['regularizer_hessian']
+        sigma = params['sigma']
+        ev = params['evidence']
+
+        print ("Evidence:", ev)
+
+        n = self.x.size()[0]
+        d = self.x.size()[1]
+
+        bSigma = torch.diag(1./torch.Tensor(ev).double()) * sigma**2
+
+        K = (self.x @ self.x.T + torch.max(H) * (bSigma))
+        y = self.y
+
+        evidence_of_the_data = -0.5*y.T@torch.linalg.solve(K,y)-0.5*torch.linalg.slogdet(2*np.pi*K)[1] ## last terms come from the other
+        return np.log(1./delta) - evidence_of_the_data
+    def prior_posterior_lr_confidence_set_cvxpy_weighted(self, theta, beta, params):
+        """
+        Return the cvxpy set constraint
+        :param theta:
+        :param beta:
+        :param params:
+        :return:
+        """
+        # create a Gaussian likelihood
+        sigma = params['sigma']
+        evidence = params['evidence']
+        print ("Evidence, in set construction:", evidence)
+        def gauss_likelihood(theta): return cp.sum(cp.multiply(np.array(evidence).reshape(-1,1),cp.square((self.x @ theta - self.y)) / (2 * (sigma ** 2))))
+        self.set_fn = lambda theta:  [gauss_likelihood(theta) <= beta + np.sum(np.array(evidence))*(1/2)*np.log(2*np.pi*sigma**2)]
+        set = self.set_fn(theta)
+        return set
+
+    def prior_posterior_lr_confidence_set_cvxpy(self, theta, beta, params):
+        """
+        Return the cvxpy set constraint
+        :param theta:
+        :param beta:
+        :param params:
+        :return:
+        """
+        # create a Gaussian likelihood
+        sigma = params['sigma']
+        evidence = params['evidence']
+        print ("Evidence, in set construction:", evidence)
+        def gauss_likelihood(theta): return cp.sum(cp.square((self.x @ theta - self.y)) / (2 * (sigma ** 2)))
+        self.set_fn = lambda theta:  [gauss_likelihood(theta) <= beta]
+        set = self.set_fn(theta)
+        return set
